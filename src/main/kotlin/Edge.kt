9d003d0475763bd83ffdaa9eb5f2188ca630870b
VladDenisov <petrushin_2002@inbox.ru>
2023-11-27 09:47:05 +0300

    Release

--- src/main/kotlin/Edge.kt
+++ src/main/kotlin/Edge.kt
@@ -1,5 +1,6 @@
 package org.guardevour.kGraphs
 
+
 data class Edge(val predecessor: String, val successor: String, val weight: Int = 1): Comparable<Edge>{
     override fun compareTo(other: Edge): Int = weight.compareTo(other.weight) //метод который настраивает сравнение по весу
 
